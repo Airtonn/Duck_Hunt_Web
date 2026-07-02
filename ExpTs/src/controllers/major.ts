ea7940b20a11134a37ecd413670e8d9eaff67548
Beatriz Jacauna <150486867+beatrizmartins123@users.noreply.github.com>
2026-07-02 17:44:41 -0400

    Update major.ts

--- ExpTs/src/controllers/major.ts
+++ ExpTs/src/controllers/major.ts
@@ -1,80 +1,45 @@
-import { type Request, type Response } from 'express';
-import { LoremIpsum } from 'lorem-ipsum';
+import type { Request, Response } from 'express';
+import type { CreateMajorDto } from '../types/major.js';
+import { createMajor, getMajors } from '../services/major.js';
 
-const lorem = new LoremIpsum({
-  sentencesPerParagraph: {
-    max: 8,
-    min: 4,
-  },
-  wordsPerSentence: {
-    max: 16,
-    min: 4,
-  },
-});
-
-const index = (req: Request, res: Response) => {
-  res.render('main/index');
+const index = async (req: Request, res: Response) => {
+  try {
+    const majors = await getMajors();
+    res.render('major/index', { majors });
+  } catch (err) {
+    res.status(500).send(err);
+  }
 };
 
-const about = (req: Request, res: Response) => {
-  res.render('main/about');
+const create = async (req: Request, res: Response) => {
+  if (req.method === 'GET') {
+    res.render('major/create');
+  } else if (req.method === 'POST') {
+    try {
+      await createMajor(req.body);
+      res.redirect('/major');
+    } catch (err) {
+      res.status(500).send(err);
+    }
+  }
 };
 
-const bemvindo = (req: Request, res: Response) => {
-  res.send(`Seja bem vindo ${req.params.nome}`);
+const read = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  res.send(id);
 };
 
-const Gerarlorem = (req: Request, res: Response) => {
-  const quantidade = parseInt(req.params.quantidade as string, 10);
-
-  const resultado = lorem.generateParagraphs(quantidade);
-
-  res.send(`<h1>Gerador de Lorem Ipsum</h1><pre>${resultado}</pre>`);
+const update = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  if (req.method === 'GET') {
+    res.render('major/update', { id });
+  }
 };
 
-const hb1 = (req: Request, res: Response) => {
-  const mensagem = 'Bem vindo(a) ao webacademy';
-  res.render('main/hb1', {
-    mensagem,
-  });
+const remove = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  res.send(id);
 };
 
-const hb2 = (req: Request, res: Response) => {
-  const message = 'Bem vindo(a) ao webacademy';
-  res.render('main/hb2', {
-    layout: 'main2',
-    showMessage: true,
-    message,
-  });
-};
+export default { index, create, read, update, remove };
 
-const hb3 = (req: Request, res: Response) => {
-  const profes = [
-    { nome: 'David Fernandes', sala: 1238 },
-    { nome: 'Horácio Fernandes', sala: 1233 },
-    { nome: 'Edleno Moura', sala: 1236 },
-    { nome: 'Elaine Harada', sala: 1231 },
-  ];
-  res.render('main/hb3', { profes });
-};
-
-const hb4 = function (req: Request, res: Response) {
-  const profes = [
-    { nome: 'David Fernandes', sala: 1238 },
-    { nome: 'Eduardo Nakamura', sala: 0 },
-    { nome: 'Edleno Moura', sala: 1236 },
-    { nome: 'Elaine Harada', sala: 1231 },
-  ];
-  res.render('main/hb4', { profes });
-};
-
-export default {
-  index,
-  about,
-  bemvindo,
-  Gerarlorem,
-  hb1,
-  hb2,
-  hb3,
-  hb4,
-};
