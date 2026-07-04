6776e6453685dc959f571c60ea376d897fb8fbdc
Airtonn <airtonsilveirafilho@gmail.com>
2026-07-03 22:48:05 -0400

    Auth.ts update

--- ExpTs/src/controllers/auth.ts
+++ ExpTs/src/controllers/auth.ts
@@ -1,34 +1,39 @@
 import type { Request, Response } from 'express';
 import { getMajors } from '../services/major.js';
-import { createUser, loginUser, logout } from '../services/auth.js';
-import type { Request, Response } from "express";
+import { createUser, checkUserPassword } from '../services/auth.js';
 
 const singup = async (req: Request, res: Response) => {
-    if (req.method === 'GET') {
-        const majors = await getMajors();
-        res.render('auth/singup', { majors });
-    } else if (req.method === 'POST') {
-        await createUser(req.body);
-        res.redirect('/login');
-        const singup = async (req: Request, res: Response) => {
-            if (req.method === "GET") {
-                res.render("auth/signup");
-            }
-        };
+  if (req.method === 'GET') {
+    const majors = await getMajors();
+    res.render('auth/signup', { majors });
+  } else if (req.method === 'POST') {
+    await createUser(req.body);
+    res.redirect('/login');
+  }
+};
 
-        const login = async (req: Request, res: Response) => {
-            if (req.method === "GET") {
-                res.render("auth/login");
-            }
-        };
-    };
+const login = async (req: Request, res: Response) => {
+  if (req.method === 'GET') {
+    res.render('auth/login');
+  } else if (req.method === 'POST') {
+    const user = await checkUserPassword(req.body);
 
-    const login = async (req: Request, res: Response) => {
-        await loginUser(req, res);
-    };
+    if (user) {
+      req.session.userId = user.id;
+      res.redirect('/');
+    } else {
+      res.render('auth/login');
+    }
+  }
+};
 
-    const logoutHandler = async (req: Request, res: Response) => {
-        await logout(req, res);
-    };
+const logoutHandler = async (req: Request, res: Response) => {
+  req.session.destroy((err) => {
+    if (err) {
+      console.error('Erro ao destruir a sessao:', err);
+    }
+    res.redirect('/login');
+  });
+};
 
-    export default { singup, login, logoutHandler };
+export default { singup, login, logoutHandler };
